073dc12478cf4e2d2cde20fe2820602d6f17f3e5
krlargo <krlargo@ucdavis.edu>
2017-11-05 23:35:54 -0800

    Add type methods

--- SyncedLists/Model/User.swift
+++ SyncedLists/Model/User.swift
@@ -9,13 +9,13 @@
 import Firebase
 import Foundation
 
-struct User {
+class User {
     var name: String;
     var email: String;
     
     // Firebase userID uses "," instead of "."
     var id: String {
-        return self.email.replacingOccurrences(of: ".", with: ",");
+        return User.emailToID(email);
     }
     
     // Constructor for Firebase-loaded User
@@ -28,5 +28,25 @@ struct User {
     init(name: String, email: String) {
         self.name = name;
         self.email = email;
+    }
+
+    // Type methods
+    class func emailToID(_ emailStr: String) -> String {
+        return emailStr.replacingOccurrences(of: ".", with: ",");
+    }
+    
+    class func nameOfUser(withEmail emailStr: String) -> String {
+        var name: String = "";
+        let userID = User.emailToID(emailStr);
+        
+        let userRef = Database.database()
+            .reference(withPath: "users")
+            .child(userID).child("name")
+            .observe(.value, with: { snapshot in
+                if let observedName = snapshot.value as? String {
+                    name = observedName;
+                }
+            });
+        return name;
     }
 }
