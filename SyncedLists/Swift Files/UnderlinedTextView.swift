197d9087d1ce3102c5efc4e79d9fb8052411079a
krlargo <krlargo@ucdavis.edu>
2017-11-09 23:32:46 -0800

    Add UnderlinedTextView subclass

---
+++ SyncedLists/Swift Files/UnderlinedTextView.swift
@@ -0,0 +1,35 @@
+// https://stackoverflow.com/questions/41486123/how-to-make-multi-lined-underlined-uitextview-uitextfield
+
+import UIKit
+
+class UnderlinedTextView: UITextView {
+    var lineHeight: CGFloat = 13.8
+    
+    override var font: UIFont? {
+        didSet {
+            if let newFont = font {
+                lineHeight = newFont.lineHeight
+            }
+        }
+    }
+    
+    override func draw(_ rect: CGRect) {
+        let ctx = UIGraphicsGetCurrentContext()
+        
+        let numberOfLines = Int(rect.height / lineHeight)
+        let topInset = textContainerInset.top
+        
+        for i in 1...numberOfLines {
+            let y = topInset + CGFloat(i) * lineHeight
+            
+            let line = CGMutablePath()
+            line.move(to: CGPoint(x: 0.0, y: y))
+            line.addLine(to: CGPoint(x: rect.width, y: y))
+            ctx?.addPath(line)
+        }
+        
+        ctx?.strokePath()
+        
+        super.draw(rect)
+    }
+}
