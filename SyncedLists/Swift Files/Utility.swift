1541defd82bf7bbed8959cfc82d6dc409d86cdcf
krlargo <krlargo@ucdavis.edu>
2017-11-09 22:11:42 -0800

    Add image rescaling function

--- SyncedLists/Swift Files/Utility.swift
+++ SyncedLists/Swift Files/Utility.swift
@@ -45,4 +45,13 @@ class Utility {
             })
         });
     }
+    
+    // MARK: Image Resize w/ Preserved Quality
+    class func scaleImage(_ image: UIImage, toSize newSize: CGSize)->UIImage{
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height));
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()?.withRenderingMode(.alwaysTemplate);
+        UIGraphicsEndImageContext()
+        return newImage!;
+    }
 }
