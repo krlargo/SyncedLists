86eada50f3acee776f0a2f84fb61ebf8c8e29f23
krlargo <krlargo@ucdavis.edu>
2017-11-09 22:11:10 -0800

    Add TabBarController.swift

--- SyncedLists/View Controllers/TabBarController.swift
+++ SyncedLists/View Controllers/TabBarController.swift
@@ -13,23 +13,17 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let myTabBarItem1 = (self.tabBar.items?[0])! as UITabBarItem
+        myTabBarItem1.image = UIImage(named: "Unselected")?.withRenderingMode(UIImageRenderingMode.alwaysOriginal)
+        myTabBarItem1.selectedImage = UIImage(named: "Selected ")?.withRenderingMode(UIImageRenderingMode.alwaysOriginal)
+        myTabBarItem1.title = ""
+        myTabBarItem1.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+
+        let listsItem = self.tabBar.items?[0];
+        listsItem?.image
+        
+        let invitesItem = self.tabBar.items?[1]!;
+        
+        let settingsItem = self.tabBar.items?[2]!;
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
