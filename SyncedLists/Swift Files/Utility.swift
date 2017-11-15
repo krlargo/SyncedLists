663e47f8f3d8e2a46f00092aafbade63a7ac81a4
krlargo <krlargo@ucdavis.edu>
2017-11-15 04:03:28 -0800

    Add transparent black background view to activity indicator

--- SyncedLists/Swift Files/Utility.swift
+++ SyncedLists/Swift Files/Utility.swift
@@ -12,26 +12,34 @@ import UIKit
 class Utility {
     // Mark: - Activity Indicator
     static var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView();
+    static var backgroundView = UIView();
     
     class func showActivityIndicator(in parentView: UIView?) {
         guard let parentView = parentView else {
             return;
         }
         
+        backgroundView = UIView();
+        backgroundView.backgroundColor = UIColor.black.withAlphaComponent(0.3);
+        backgroundView.layer.cornerRadius = 10;
+        backgroundView.frame = CGRect(x: 0, y: 0, width: 75, height: 75);
+        backgroundView.center = CGPoint(x: parentView.frame.midX, y: parentView.frame.midY);
+        
+        activityIndicator = UIActivityIndicatorView();
         activityIndicator.activityIndicatorViewStyle = .whiteLarge;
-        activityIndicator.center = CGPoint(x: 0, y: 0);
         activityIndicator.color = UIColor(displayP3Red: 66/255, green: 178/255, blue: 91/255, alpha: 1.0);
-        activityIndicator.frame = CGRect(x: parentView.frame.midX,
-                                         y: parentView.frame.midY,
-                                         width: 0, height: 0);
+        activityIndicator.center = CGPoint(x: backgroundView.frame.width/2,
+                                           y: backgroundView.frame.height/2);
         activityIndicator.hidesWhenStopped = true;
-
-        parentView.addSubview(activityIndicator);
+        
+        backgroundView.addSubview(activityIndicator);
+        parentView.addSubview(backgroundView);
+        
         activityIndicator.startAnimating();
     }
     
     class func hideActivityIndicator() {
-        activityIndicator.stopAnimating();
+        backgroundView.removeFromSuperview();
     }
     
     // Mark: - AutoDismissing Alert Message
